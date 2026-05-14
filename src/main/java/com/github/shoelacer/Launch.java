c19077a50cb2ed79d20788f9b258f69e5483532f
Shoelacer <139281278+Shoelacer@users.noreply.github.com>
2026-05-14 15:53:58 -0500

    Initial commit: JavaFX LinAlg Visualizer

---
+++ src/main/java/com/github/shoelacer/Launch.java
@@ -0,0 +1,29 @@
+package com.github.shoelacer;
+
+import com.github.shoelacer.windows.StartWindow;
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+import java.util.Scanner;
+
+public class Launch extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage window) {
+        window.setScene(new Scene(new StartWindow().getPane()));
+        window.show();
+    }
+}
---
+++ src/main/java/com/github/shoelacer/windows/StartWindow.java
@@ -0,0 +1,14 @@
+package com.github.shoelacer.windows;
+
+import javafx.scene.layout.StackPane;
+
+public class StartWindow {
+
+
+    public StackPane getPane(){
+        StackPane pane = new StackPane();
+
+        return pane;
+    }
+
+}
